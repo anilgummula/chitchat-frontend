50000d832725f677e0dae9eda1fa3af7658a9189
anil gummula <anilgummula2060@gmail.com>
2025-02-27 04:14:42 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { handleError, handleSuccess } from '../utils';
 
 const Register = () => {
     const [userInfo,setUserInfo] = useState({});
-    const [name,setName] = useState(null);
-    const [email,setEmail] = useState(null);
-    const [mobile,setMobile] = useState(null);
-    const [password,setPassword] = useState(null);
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [mobile,setMobile] = useState("");
+    const [password,setPassword] = useState("");
 
     const [button,setButton] = useState(true);
+    const navigate = useNavigate();
 
     const handleSubmit = (e)=>{
         e.preventDefault();
-        if(userInfo){
+        setButton(false);
+
+        if(!name ||!email  || !mobile || !password){
+            handleError("Fill the form properly bro!");
+            setButton(true);
+            return 
+        }
+        else{
             setUserInfo(
                 {
                     "name":name,
@@ -21,18 +30,24 @@ const Register = () => {
                     "password":password
                 }
             )
+            handleSuccess("Registeration success...!")
+            setTimeout(() => {
+                navigate("/chat");
+            }, 1000);
         }
+
     }
 
   return (
     <>
         <div className='min-h-screen w-full bg-black flex justify-center text-center px-6'>
-            <form action={(e)=>{handleSubmit(e)}} className=' m-auto border-2 border-gray-400 rounded-lg text-gray-300 md:p-10 p-8  items-center space-y-8  '>
+            <form onSubmit={handleSubmit} className=' m-auto border-2 border-gray-400 rounded-lg text-gray-300 md:p-10 p-8  items-center space-y-8  '>
                 <h1>Register now!</h1>
                 <label htmlFor="name">Name</label>
                 <input className='border mx-4 rounded-md h-[32px] px-2 text-slate-400 outline-none border-none bg-white/10'
                     type="text"
                     required 
+                    value={name}
                     onChange={(e)=> {setName(e.target.value)}} 
                 />
                 <br />
@@ -40,6 +55,7 @@ const Register = () => {
                 <input  className='border mx-4 rounded-md h-[32px] px-2 text-slate-400 outline-none border-none bg-white/10'
                     type="text" 
                     required 
+                    value={mobile}
                     onChange={(e)=> {setMobile(e.target.value)}}
                 />
                 <br />
@@ -47,6 +63,7 @@ const Register = () => {
                 <input className='border mx-4 rounded-md h-[32px] px-2 text-slate-400 outline-none border-none bg-white/10'
                     type="text" 
                     required 
+                    value={email}
                     onChange={(e)=> {setEmail(e.target.value)}}
                 />   
                 <br />
@@ -54,6 +71,7 @@ const Register = () => {
                 <input className='border mx-4 rounded-md h-[32px] px-2 text-slate-400 outline-none border-none bg-white/10'
                     type="text" 
                     required 
+                    value={password}
                     onChange={(e)=> {setPassword(e.target.value)}}
                 />
                 <br />
@@ -63,7 +81,7 @@ const Register = () => {
                 <input className={`border bg-white/70 hover:bg-white/60 text-black font-semibold p-1 px-2 rounded-md ${!button ? 'opacity-50 cursor-not-allowed' : ''} flex  mx-auto `}
                     type="submit" 
                     value={button ? "Register" : "Please wait...."} 
-                    onClick={(e)=> setButton(false)} 
+                    // onClick={(e)=> setButton(false)} 
                     disabled={!button}  
                 />   
 
