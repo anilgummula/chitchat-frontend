87131fb5de892304cb55e9a6efc627134c7c2af7
anil gummula <anilgummula2060@gmail.com>
2025-02-27 04:17:47 +0530

    updated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const Profile = () => {
   return (
-    <div className='bg-black text-white min-h-screen flex flex-col mx-auto justify-center items-center p-4'>
+    <div className='bg-black text-white min-h-screen flex flex-col mx-auto justify-center items-center p-4 space-y-10'>
         <p className='p-4 text-red-300'>Profile not found!</p>
         <Link to={'/'} className='text-yellow-300 border p-2 rounded-lg hover:cursor-pointer'>Logout</Link>
     </div>
